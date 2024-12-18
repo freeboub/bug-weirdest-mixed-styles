a397346c79f402c8c95cf9a35563fe7504bc77e6
Olivier Bouillet <olivier@azzapp.com>
2024-12-18 22:31:21 +0100

    feat: remove expo

--- App.tsx
+++ App.tsx
@@ -1,118 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Screen, ScreenStack } from 'react-native-screens';
+import type { Dispatch, SetStateAction } from 'react';
+import ParentScreen from './ParentScreen';
+import ChildScreen from './ChildScreen';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export type ScreensRendererProps = {
+  index: number;
+  setArray: Dispatch<SetStateAction<number[]>>;
+};
+const screens = [ParentScreen, ChildScreen];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export const ScreenRenderer = ({ index, setArray }: ScreensRendererProps) => {
+  const Component: any = screens[index];
+  const [willAppear, setWillAppear] = useState(true);
+  const [disappear, setDisappear] = useState(true);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <Screen
+      key={index}
+      activityState={2}
+      onWillAppear={() => setWillAppear(c => !c)} // Need to be on both screen to reproduce the issue
+      onDisappear={() => setDisappear(c => !c)} // Need to be on both screen to reproduce the issue
+      style={StyleSheet.absoluteFill}
+    >
+      <Component
+        setArray={setArray}
+        willAppear={willAppear}
+        disappear={disappear}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    </Screen>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const App = () => {
+  // This array represent the screen stack
+  // It is either
+  // [1] only Parent screen displayed
+  // or [1, 2] Parent and Child Screen are in the stack
+  const [array, setArray] = useState([1]);
+
+  return (
+    <ScreenStack style={{ flex: 1 }}>
+      {array.map((r, index) => {
+        return <ScreenRenderer key={index} index={index} setArray={setArray} />;
+      })}
+    </ScreenStack>
+  );
+};
 
 export default App;
